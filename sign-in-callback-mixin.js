b3b58eec9e4a9a2fd0176e59242875aa4821cbcc
Dominik Wachter <wachterdominik@gmail.com>
2020-04-11 21:53:27 +0200

    Addded cancel method to callback mixins

--- sign-in-callback-mixin.js
+++ sign-in-callback-mixin.js
@@ -2,5 +2,10 @@ export default {
   async created() {
     const path = await this.$store.dispatch('auth/signInCallback')
     this.$router.push(path)
+  },
+  methods: {
+    cancel() {
+      this.$router.push('/')
+    }
   }
 }
--- sign-out-callback-mixin.js
+++ sign-out-callback-mixin.js
@@ -2,5 +2,10 @@ export default {
   async created() {
     await this.$store.dispatch('auth/signOutCallback')
     this.$router.push('/')
+  },
+  methods: {
+    cancel() {
+      this.$router.push('/')
+    }
   }
 }
